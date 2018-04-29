05a9ae3670ffaf989fc2d38ad724f99b2c196d0f
Chris Doran <chrisdoran@protonmail.com>
2018-04-29 18:36:57 +1000

    randomly generate obstacles

--- js/gauntlet.js
+++ js/gauntlet.js
@@ -1,5 +1,6 @@
 // Application logic goes here
 var scene = new THREE.Scene();
+
 /*args: FOV(degrees), aspect ratio, near clipping plane, far clipping plane (point which rendering ceases) */
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 3, -1)
@@ -19,10 +20,9 @@ var color = new THREE.Color(0xCCFFFF);
 scene.background = color;
 
 //PLANE
-// A mesh is created from the geometry and material, then added to the scene
 var plane = new THREE.Mesh(
   new THREE.PlaneGeometry(500, 500, 100, 100),
-  new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: true })
+  new THREE.MeshBasicMaterial({ color: 0xffffff, side:THREE.DoubleSide, wireframe: true })
 );
 plane.rotation.x = Math.PI / 2;
 scene.add(plane);
@@ -46,8 +46,28 @@ scene.add(light);
 var ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-camera.position.z = 5;
+// OBSTACLES
 
+// Use this to randgen object positions
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+var obstacleGeo = new THREE.BoxGeometry(3, 1, 1);
+var obstacleMat = new THREE.MeshLambertMaterial({color: 0xD3D3D3});
+let obstacle = new THREE.Mesh(obstacleGeo, obstacleMat);
+let num;
+
+// add n obstacles to the scene qasi randomly
+for (num = 0; num < 30; num++) {
+  let newObs = new THREE.Mesh(obstacleGeo, obstacleMat);
+  newObs.position.x = getRandomInt(-20, 20)
+  newObs.position.z = getRandomInt(0, -500)
+  newObs.position.y = 0.5
+  scene.add(newObs)
+}
 
 var keys = {
   left: false,
@@ -56,7 +76,6 @@ var keys = {
 //Event listeners for keypress
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  console.log(keyName)
   if (keyName === 'a') {
     keys.left = true;
   } 
@@ -67,7 +86,6 @@ document.addEventListener('keydown', (event) => {
 
 document.addEventListener('keyup', (event) => {
   const keyName = event.key;
-  console.log(keyName)
   if (keyName === 'a') {
     keys.left = false;
   } 
